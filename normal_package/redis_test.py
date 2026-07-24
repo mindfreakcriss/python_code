8834ae82a7661937bed5c3ed3d6e8de933dff37a
crissHuang <735884048@qq.com>
2026-07-24 14:17:37 +0800

    【提交】提交redis的操作

---
+++ normal_package/redis_test.py
@@ -0,0 +1,15 @@
+import redis
+
+#直连方式
+#r = redis.Redis(host='127.0.0.1',port=6379,db=0)
+
+#连接池
+pool = redis.ConnectionPool(host='127.0.0.1', port=6379,db=0)
+r = redis.Redis(connection_pool=pool)
+
+r.set('first_key','test')
+keys = r.keys()
+print(keys)
+
+value = r.get('first_key')
+print(value)
